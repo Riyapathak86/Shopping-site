80bcd5da30f56f8b8cfb4469e072722ef7fcb6b5
Riyapathak86 <Ruhiriya86@gmail.com>
2025-12-07 22:51:36 +0530

    Resolved merge conflicts in Cart.jsx

--- ecom/src/contextState/ecomState.jsx
+++ ecom/src/contextState/ecomState.jsx
@@ -6,22 +6,25 @@ const EcomState = ({ children }) => {
   // Initialize state with items
   const [filteredProducts, setFilteredProducts] = useState(items);
   const [searchTerm, setSearchTerm] = useState("");
-  const [cartArry ,setcartArry]=useState([]);
+  const [cartArry ,setcartArry] = useState([]);
   const [cartno, setcartno] = useState(0);
 
-const x=8;
+  const x = 8;
+
   return (
-    <ecomContext.Provider value={{  items,  filteredProducts, setFilteredProducts ,
-<<<<<<< HEAD
-      searchTerm, setSearchTerm,x
-=======
-      searchTerm, setSearchTerm, setSearchTerm,x
->>>>>>> 6403659a0bcddfe866833c7b572ff8200ffe4ace
-      ,cartArry ,setcartArry,
-      cartno, setcartno,
+    <ecomContext.Provider value={{  
+        items,  
+        filteredProducts, 
+        setFilteredProducts,
+        searchTerm, 
+        setSearchTerm,
+        x,
+        cartArry,
+        setcartArry,
+        cartno, 
+        setcartno,
     }}>
-      {children} 
-      
+      {children}
     </ecomContext.Provider>
   );
 };
